a86065001fb28595d094da3858bb2f3d368ff2be
Arkham <liuju86@gmail.com>
2009-03-13 04:04:20 +0100

    Small fixes to bashrc; auto-backup script first version.

--- .bashrc
+++ .bashrc
@@ -1,5 +1,23 @@
 ## Fancy prompt
-PS1="\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[m\] \[\e[1;32m\]\$ \[\e[m\] "
+red='\[\e[0;31m\]'
+RED='\[\e[1;31m\]'
+blue='\[\e[0;34m\]'
+BLUE='\[\e[1;34m\]'
+cyan='\[\e[0;36m\]'
+CYAN='\[\e[1;36m\]'
+black='\[\e[0;30m\]'
+BLACK='\[\e[1;30m\]'
+green='\[\e[0;32m\]'
+GREEN='\[\e[1;32m\]'
+yellow='\[\e[0;33m\]'
+YELLOW='\[\e[1;33m\]'
+magenta='\[\e[0;35m\]'
+MAGENTA='\[\e[1;35m\]'
+white='\[\e[0;37m\]'
+WHITE='\[\e[1;37m\]'
+NC='\[\e[0m\]' # No Color
+# 
+PS1="${green}\u${NC} ${BLUE}\w${NC} ${GREEN}\$ ${NC} "
 
 ## Bash completion
 if [ -f /etc/bash_completion ]; then
---
+++ dotfiles.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Dot files backup & deploy script
+
+dotfiles=( ~/.bashrc \
+            /etc/X11/xorg.conf \
+            /etc/rc.conf \
+            /etc/syslog-ng.conf )
+
+ACTION=${1:-"backup"}
+
+case "$ACTION" in
+    backup)
+        for x in "${dotfiles[@]}"; do
+            # Check if the version differs
+            diff "$x" "`basename $x`" &> /dev/null
+            if [ $? -gt 0 ]; then
+                echo "Backing up $x ..."
+                cp "$x" "`basename $x`"
+            fi
+        done
+        echo "Backup completed."
+        ;;
+    *)
+        echo "usage: $0 {backup|deploy}"
+esac
