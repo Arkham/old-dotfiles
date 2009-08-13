93598d6f0b02ac255a560cc4a31cbaa0e66d67bb
Arkham <liuju86@gmail.com>
2009-08-14 01:22:55 +0200

    thesis: updated to (14)
    Signed-off-by: Arkham <liuju86@gmail.com>

--- thesis/save.sh
+++ thesis/save.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp /home/arkham/Desktop/thesis/ju_liu_thesis.tex .
-cp -r /home/arkham/Desktop/thesis/img .
+cp -v /home/arkham/Desktop/thesis/ju_liu_thesis.tex .
+cp -rv /home/arkham/Desktop/thesis/img .
