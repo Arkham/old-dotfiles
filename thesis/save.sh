44e5360f92582f33b9d5fafec9f4822b50ff83ad
Arkham <liuju86@gmail.com>
2009-08-28 02:53:58 +0200

    thesis: added presentation
    Signed-off-by: Arkham <liuju86@gmail.com>

--- thesis/save.sh
+++ thesis/save.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp -v /home/arkham/Desktop/thesis/ju_liu_thesis.tex .
+cp -v /home/arkham/Desktop/thesis/ju_liu_presentation.tex .
 cp -v /home/arkham/Desktop/thesis/bibliography.bib .
 cp -v /home/arkham/Desktop/thesis/is-alpha.bst .
 cp -rv /home/arkham/Desktop/thesis/img .
