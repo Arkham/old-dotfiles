15e767fdc89343558fc3e7d4f9ad79a0ba4cd428
Arkham <liuju86@gmail.com>
2009-10-19 03:17:26 +0200

    thesis: updated to Poli 1.0
    Signed-off-by: Arkham <liuju86@gmail.com>

--- thesis/save.sh
+++ thesis/save.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cp -v /home/arkham/Desktop/thesis/ju_liu_thesis.tex .
+cp -v /home/arkham/Desktop/thesis/ju_liu_thesis.toc .
+cp -v /home/arkham/Desktop/thesis/logokth.eps .
+cp -v /home/arkham/Desktop/thesis/logopolito.pdf .
 cp -v /home/arkham/Desktop/thesis/ju_liu_presentation.tex .
 cp -v /home/arkham/Desktop/thesis/bibliography.bib .
 cp -v /home/arkham/Desktop/thesis/is-alpha.bst .
