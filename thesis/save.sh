55d00ec7288679fb308c467d1cf0da038cd0eb02
Arkham <liuju86@gmail.com>
2009-10-22 17:13:04 +0200

    thesis: summary RC
    Signed-off-by: Arkham <liuju86@gmail.com>

--- thesis/save.sh
+++ thesis/save.sh
@@ -4,6 +4,7 @@ cp -v /home/arkham/Desktop/thesis/ju_liu_thesis.toc .
 cp -v /home/arkham/Desktop/thesis/logokth.eps .
 cp -v /home/arkham/Desktop/thesis/logopolito.pdf .
 cp -v /home/arkham/Desktop/thesis/ju_liu_presentation.tex .
+cp -v /home/arkham/Desktop/thesis/ju_liu_summary.tex .
 cp -v /home/arkham/Desktop/thesis/bibliography.bib .
 cp -v /home/arkham/Desktop/thesis/is-alpha.bst .
 cp -rv /home/arkham/Desktop/thesis/img .
